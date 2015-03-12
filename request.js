c47fcda3e13a9a0f858598657486cd6f29ee9f0d
Gang Peng <gapeng@microsoft.com>
2015-03-11 19:52:30 -0700

    Fix some indent problem

--- request.js
+++ request.js
@@ -5,13 +5,12 @@ request.get('https://www.yammer.com/api/v1/users/current.json', {
     'bearer': '[your_access_token]'
   }
 }).on('response', function(response) {
-    response.body = "";
-    response.on('data', function(chunk){
-      response.body += chunk;
-    });
-    response.on('end', function(){
-      console.log(response.statusCode); // 200
-      console.log(response.body); // 'image/png'
-    });
-
+  response.body = "";
+  response.on('data', function(chunk){
+    response.body += chunk;
   });
+  response.on('end', function(){
+    console.log(response.statusCode);
+    console.log(response.body);
+  });
+});
