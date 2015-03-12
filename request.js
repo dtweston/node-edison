655223c2788bb1c26a961d8ad4baeac7b4c3c0a1
Gang Peng <gapeng@microsoft.com>
2015-03-11 18:01:30 -0700

    Make https get request to yammer

---
+++ request.js
@@ -0,0 +1,17 @@
+var request = require('request');
+
+request.get('https://www.yammer.com/api/v1/users/current.json', {
+  'auth': {
+    'bearer': '[your_access_token]'
+  }
+}).on('response', function(response) {
+    response.body = "";
+    response.on('data', function(chunk){
+      response.body += chunk;
+    });
+    response.on('end', function(){
+      console.log(response.statusCode); // 200
+      console.log(response.body); // 'image/png'
+    });
+
+  });
