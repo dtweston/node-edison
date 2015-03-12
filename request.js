d5cb3f984aee4a7f085455c507ebabcf6be8f479
Cheuk Dong <cdong@yammer-inc.com>
2015-03-12 11:32:05 -0700

    make parsing message body work

--- request.js
+++ request.js
@@ -1,8 +1,11 @@
 var request = require('request');
 
-request.get('https://www.yammer.com/api/v1/users/current.json', {
+
+//https://www.yammer.com/api/v1/messages/inbox.json?filter=unarchived%3Binbox_unseen&threaded=extended&exclude_own_messages_from_unseen=true&_=1426184657399
+
+request.get('https://www.yammer.com/api/v1/messages/inbox.json?filter=unarchived%3Binbox_unseen&threaded=extended&exclude_own_messages_from_unseen=true', {
   'auth': {
-    'bearer': '[your_access_token]'
+    'bearer': 'lbbRDdqRosJ3e8EFuAr4xg'
   }
 }).on('response', function(response) {
   response.body = "";
@@ -10,7 +13,14 @@ request.get('https://www.yammer.com/api/v1/users/current.json', {
     response.body += chunk;
   });
   response.on('end', function(){
-    console.log(response.statusCode);
-    console.log(response.body);
+    //console.log(response.statusCode);
+    //console.log(response.body);
+    var jsonMessages = JSON.parse(response.body);
+    //console.log(jsonMessages);
+    var messages = [];
+    for (var i=0; i < jsonMessages.messages.length; i++) {
+      messages[i] = jsonMessages.messages[i].body.plain;
+      console.log(messages[i]);
+    }
   });
 });
